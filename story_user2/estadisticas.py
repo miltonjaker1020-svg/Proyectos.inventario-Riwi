3a2d6ba9b980df09a7a501f948d46602166f0e74
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-18 18:27:43 -0500

    Update estadisticas.py

--- story_user2/estadisticas.py
+++ story_user2/estadisticas.py
@@ -1,21 +1,22 @@
-from mostrar import *
+from mostrar import * # Inportamos las funciones del archivo mostrar
 
 def calular_estadisticas(inventario):
-    print( "\033[94m" + """
+    print( "\033[94m" + """                                                                 
                             ============================================
                             ===bienvenido al programa de estadisticas===                 
                             ============================================
                             
-                            """+ "\033[0m" )
-    if not inventario:
+                            """+ "\033[0m" ) # Imprimimos una bienvenida
+
+    if not inventario: # Verificamos que hayan valores en el inventario
         print("\033[91m" + "no hay productos en el inventario"+ "\033[0m")
         
-    total_cantidad=0
-    total_precio=0
+    total_cantidad=0 # Establecemos acumuladores 
+    total_precio=0 
     
     for i in inventario:
-        total_cantidad+=i['cantidad']
+        total_cantidad+=i['cantidad'] # acumulamos los valores
         total_precio+=i['precio']
 
-    print("cantidad total de productos: ",total_cantidad)
+    print("cantidad total de productos: ",total_cantidad) # Imprimimos los valores acumulados
     print("precio total de productos: ",total_precio*total_cantidad)
