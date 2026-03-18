daff1cd842de43d9c71f38afcf3e1dbb0a6eb235
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-18 18:39:13 -0500

    Update main.py

--- story_user2/main.py
+++ story_user2/main.py
@@ -1,14 +1,14 @@
 from estadisticas import *
 
 
-arranque=True
+arranque=True  # Establecemos la variable junto con el valor true para iniciar el ciclo
 while arranque==True:
     print( "\033[94m" + """
                     ==========================================
                     ===bienvenido al programa de inventario===
                     ==========================================
                     
-                    """+ "\033[0m")
+                    """+ "\033[0m") # Imprimimos el mensaje de bienvenida
     
     print("""
              1. para agregar productos al inventario
@@ -16,14 +16,14 @@ while arranque==True:
              3. para ver las estadisticas del inventario
              4. salir
              
-             """)
+             """) # Mostramos las opciones
     
-    opcion=int(input("ingrese una opcion: "))
-    if opcion==1:
+    opcion=int(input("ingrese una opcion: ")) # Guardamos la opcion escogida
+    if opcion==1: # Si la opcion es 1 esto iniciara la funcion agregar_producto
         agregar_producto()
-    elif opcion==2:
+    elif opcion==2: # Si la opcion es 2 esto iniciara la funcion mostrar_inventario
         mostrar_inventario(inventario)
-    elif opcion==3:
+    elif opcion==3: # Si la opcion es 3 esto iniciara la funcion calcular_estadisticas
         calular_estadisticas(inventario)
-    elif opcion==4:
+    elif opcion==4: # Si la opcion es 4 esto ara que arranque sea false y asi cerrar el programa
         arranque=False
