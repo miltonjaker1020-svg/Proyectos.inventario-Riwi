111bd2900e4da4c6d435bbe96eb173078ef36e5f
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-18 17:47:00 -0500

    Update aggproduct.py

--- story_user2/aggproduct.py
+++ story_user2/aggproduct.py
@@ -15,10 +15,10 @@ def agregar_producto():
     ROJO = "\033[91m"
     
    
-    confirmar=True
+    confirmar=True #Establesco la variable confirmar y que esta contenga un true para no usar el while true
     while confirmar==True:
         
-    #mensaje de bienvenida
+    #Mensaje de bienvenida
         print(AZUL + """
             ===================================================
             === BIENVENIDO AL APARTADO DE AGREGAR PRODUCTOS ===
@@ -27,54 +27,54 @@ def agregar_producto():
             """+ RESET)
     
     
-    #ingreso del producto
+    #Ingreso del producto
         producto=input("ingrese el nombre del producto: ")
     
-    #validacion de que el producto sea un string y tmabien que no este vacio
+    #Validacion de que el producto sea un string y tmabien que no este vacio
         while producto=="" or not producto.isalpha():
             print(ROJO + "ingrese un producto valido"+ RESET)
             producto=input("ingrese el nombre del producto: ")
         
         try: 
-            cantidad=int(input("ingrese la cantidad del producto: "))
-            while cantidad <= 0:
+            cantidad=int(input("ingrese la cantidad del producto: ")) # Pedimos la cantidad del producto
+            while cantidad <= 0: # Validamos que no sea un numero negativo o 0
                 print(ROJO + "ingrese una cantidad valida"+ RESET)
                 cantidad=int(input("ingrese la cantidad de productos: "))
         
         
-            precio=float(input("ingrese el precio  de los productos: "))
-            while precio <= 0:
+            precio=float(input("ingrese el precio  de los productos: ")) # Pedimos el precio del producto
+            while precio <= 0: # Validamos que no sea un numero negativo o menor a 0
                 print(ROJO + "ingrese un precio valido"+ RESET)
                 precio=float(input("ingrese el precio  del producto: "))
 
            
-            if producto in inventario:
+            if producto in inventario:# Establecemos que si el producto ya existe este sobre escriba la lista
                 inventario[producto]["cantidad"]+=cantidad
                 totalactualizado=inventario[producto]["precio"]/inventario[producto]["cantidad"]
                 inventario[producto]["precio"]+=totalactualizado*cantidad
-            else:
+            else: # Se establece el diccionario
                 diccionario={
                 "producto":producto,    
                 "cantidad":cantidad,
                 "precio":precio
                 
              }
-                inventario.append(diccionario)
+                inventario.append(diccionario) # Guarda el diccionario en la lista inventario
             
            
                 
-            print(VERDE + "✔ producto agregado con exito"+ RESET)
-            confirmar=input("desea agregar otro producto? (si/no): ").lower()
-            while confirmar not in ("si","no"):
+            print(VERDE + "✔ producto agregado con exito"+ RESET) # Le hacemos saber al cliente que se registro correctamente el producto
+            confirmar=input("desea agregar otro producto? (si/no): ").lower()  # Preguntamos si quiere agregar otro producto
+            while confirmar not in ("si","no"): # Verificamos que no pongo palabras incorrectas
                 print(ROJO + "ingrese un valor valido"+ RESET)
                 confirmar=input("desea agregar otro producto? (si/no): ").lower()
             
-            if confirmar=="no":
+            if confirmar=="no":  # Si pone no cerramos el siclo con un confirmar false
                 confirmar=False
                 
-            elif confirmar=="si":
+            elif confirmar=="si": # Si pone si inicia otra iteracion con un confirmar true
                 confirmar=True
 
-        except ValueError:
+        except ValueError:  # Capturamos el error
             print(ROJO + "ingrese un valor valido"+ RESET)
             continue
