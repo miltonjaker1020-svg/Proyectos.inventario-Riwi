30167e32304702d0e802d385bc0ef28cb87b2f5c
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-18 18:46:33 -0500

    Update mostrar.py

--- story_user2/mostrar.py
+++ story_user2/mostrar.py
@@ -1,20 +1,20 @@
 from aggproduct import *
 
-def mostrar_inventario(inventario):
-    run=True
-    while run==True:
+def mostrar_inventario(inventario): # Establecemos la funcion mostrar_inventario
+    run=True # Establecemos el iniciador
+    while run==True: 
         print("\033[94m" + """
               ===================================================
               === BIENVENIDO AL APARTADO DE AGREGAR PRODUCTOS ===
               ===================================================
     
-              """+ "\033[0m" )
-        if not inventario:
+              """+ "\033[0m" ) # Imprimimos la bienvenida
+        if not inventario: # Si el inventario esta vacio imprime un mensaje que dice que esta vacio
             print("\033[91m" + "no hay productos en el inventario"+ "\033[0m")
             
         else:
-            for i in inventario:
+            for i in inventario: # lee los valores que estan dentro de la lista
                 print(f"producto: {i["producto"]}  |  cantidad: {i['cantidad']}  |  precio total : {i['precio']} ")
-        run=False
+        run=False # Simula un break
         
         
