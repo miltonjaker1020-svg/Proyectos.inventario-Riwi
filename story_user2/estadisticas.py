965ade475f7902ca313099b2cefa5202f2413314
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-20 16:33:32 -0500

    Update estadisticas.py

--- story_user2/estadisticas.py
+++ story_user2/estadisticas.py
@@ -11,25 +11,29 @@ def calular_estadisticas(inventario):
           1. ver valor total del inventario
           2. ver la cantidad total de productos registrados
           """)
-    try:
-        eleccion=int(input("ingrese la opcion que desea escoger"))
-        if not inventario: # Verificamos que hayan valores en el inventario
+    if not inventario: # Si el inventario esta vacio imprime un mensaje que dice que esta vacio
             print("\033[91m" + "no hay productos en el inventario"+ "\033[0m")
+    
+    else:
+        try:
+            eleccion=int(input("ingrese la opcion que desea escoger"))
+            if not inventario: # Verificamos que hayan valores en el inventario
+                print("\033[91m" + "no hay productos en el inventario"+ "\033[0m")
+                
+            total_cantidad=0 # Establecemos acumuladores 
+            total_precio=0 
             
-        total_cantidad=0 # Establecemos acumuladores 
-        total_precio=0 
-        
-        for i in inventario:
-            total_cantidad+=i['cantidad'] # acumulamos los valores
-            total_precio+=i['precio']
+            for i in inventario:
+                total_cantidad+=i['cantidad'] # acumulamos los valores
+                total_precio+=i['precio']
 
-        if eleccion==1:
-        
-            print("cantidad total de productos: ",total_cantidad) # Imprimimos los valores acumulados
-
-        elif eleccion==2:
-            print("precio total de productos: $",total_precio*total_cantidad) 
+            if eleccion==1:
             
-    except ValueError:
-        print("\033[91m" + "error coloque una opcion valida" + "\033[0m")
-        eleccion=int(input("ingrese la opcion que desea escoger"))
+                print("cantidad total de productos: ",total_cantidad) # Imprimimos los valores acumulados
+
+            elif eleccion==2:
+                print("precio total de productos: $",total_precio*total_cantidad) 
+                
+        except ValueError:
+            print("\033[91m" + "error coloque una opcion valida" + "\033[0m")
+            eleccion=int(input("ingrese la opcion que desea escoger"))
