1d3a89f154d8529b644d67041e519de1b214cc7a
Milton-Ortega <miltonjaker1020@gmail.com>
2026-03-20 12:37:48 -0500

    Update main.py

--- story_user2/main.py
+++ story_user2/main.py
@@ -17,13 +17,22 @@ while arranque==True:
              4. salir
              
              """) # Mostramos las opciones
+    try:
+        opcion=int(input("ingrese una opcion: "))   # Guardamos la opcion escogida
     
-    opcion=int(input("ingrese una opcion: ")) # Guardamos la opcion escogida
-    if opcion==1: # Si la opcion es 1 esto iniciara la funcion agregar_producto
-        agregar_producto()
-    elif opcion==2: # Si la opcion es 2 esto iniciara la funcion mostrar_inventario
-        mostrar_inventario(inventario)
-    elif opcion==3: # Si la opcion es 3 esto iniciara la funcion calcular_estadisticas
-        calular_estadisticas(inventario)
-    elif opcion==4: # Si la opcion es 4 esto ara que arranque sea false y asi cerrar el programa
-        arranque=False
+        while opcion<1 or opcion>4: 
+            # Si la opcion es menor a 1 o mayor a 4 esto imprimira un mensaje
+            print("\033[91m" + "ingrese una opcion valida"+ "\033[0m")
+            opcion=int(input("ingrese una opcion: "))
+    
+        if opcion==1: # Si la opcion es 1 esto iniciara la funcion agregar_producto
+            agregar_producto()
+        elif opcion==2: # Si la opcion es 2 esto iniciara la funcion mostrar_inventario
+            mostrar_inventario(inventario)
+        elif opcion==3: # Si la opcion es 3 esto iniciara la funcion calcular_estadisticas
+            calular_estadisticas(inventario)
+        elif opcion==4: # Si la opcion es 4 esto ara que arranque sea false y asi cerrar el programa
+            arranque=False
+
+    except ValueError:
+        print("\033[91m" + "ingrese una opcion valida"+ "\033[0m")
